d1fb2dacb29fc0e9ee61345e5336026ed236dad8
medina5 <emmanuel.medina@univ-lorraine.fr>
2025-02-08 15:07:59 +0100

    depots 25

--- jardins/seed.sql
+++ jardins/seed.sql
@@ -1,3 +1,7 @@
+\COPY tournees FROM 'tournees.csv' (FORMAT CSV, header, ENCODING 'UTF8');
 \COPY adherents(adherent_id,jardin_id,adherent,depot_id) FROM 'adherents.csv' (FORMAT CSV, header, ENCODING 'UTF8');
 \COPY abonnements(abonnement_id,adherent_id,panier_id) FROM 'abonnements-2025.csv' (FORMAT CSV, header, ENCODING 'UTF8');
+\COPY distributions FROM 'distributions.csv' (FORMAT CSV, header, ENCODING 'UTF8');
+\COPY plannings FROM 'plannings-2025.csv' (FORMAT CSV, header, ENCODING 'UTF8');
+\COPY propositions FROM 'propositions-2025.csv' (FORMAT CSV, header, ENCODING 'UTF8');
 \COPY livraisons_import FROM 'livraisons-2025.csv' (FORMAT CSV, header, ENCODING 'UTF8');
