5fac93c23d7c883d300f82912e38a48c9bfdc121
medina5 <emmanuel.medina@univ-lorraine.fr>
2025-02-09 20:58:06 +0100

    adresse

--- jardins/backup.sql
+++ jardins/backup.sql
@@ -1,5 +1,5 @@
-\COPY adherents(adherent_id,jardin_id,adherent,profil_id,depot_id,email,date_sortie) TO 'data/adherents.csv' (FORMAT CSV, header, ENCODING 'UTF8');
+\COPY adherents(adherent_id,jardin_id,adherent,profil_id,depot_id,email,date_sortie,adresse_id) TO 'data/adherents.csv' (FORMAT CSV, header, ENCODING 'UTF8');
 \COPY plannings TO 'data/plannings-2025.csv' (FORMAT CSV, header, ENCODING 'UTF8');
 \COPY tournees TO 'data/tournees.csv' (FORMAT CSV, header, ENCODING 'UTF8');
 \COPY distributions TO 'data/distributions.csv' (FORMAT CSV, header, ENCODING 'UTF8');
-\COPY abonnements TO 'data/abonnements-2025.csv' (FORMAT CSV, header, ENCODING 'UTF8');
+\COPY abonnements(abonnement_id,adherent_id,panier_id) TO 'data/abonnements-2025.csv' (FORMAT CSV, header, ENCODING 'UTF8');
