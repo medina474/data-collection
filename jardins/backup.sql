55f5dc51d2fe226f06e7ce355a2307269084f41a
medina5 <emmanuel.medina@univ-lorraine.fr>
2025-02-09 18:38:10 +0100

    adherents date sortie

--- jardins/backup.sql
+++ jardins/backup.sql
@@ -1,3 +1,4 @@
+\COPY adherents(adherent_id,jardin_id,adherent,profil_id,depot_id,email,date_sortie) TO 'data/adherents.csv' (FORMAT CSV, header, ENCODING 'UTF8');
 \COPY plannings TO 'data/plannings-2025.csv' (FORMAT CSV, header, ENCODING 'UTF8');
 \COPY tournees TO 'data/tournees.csv' (FORMAT CSV, header, ENCODING 'UTF8');
 \COPY distributions TO 'data/distributions.csv' (FORMAT CSV, header, ENCODING 'UTF8');
